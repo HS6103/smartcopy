e897aa79bb0e58b38ec8cb5b433ffcaffdb27bd4
HS6103 <hsuchen1022@gmail.com>
2025-07-26 21:45:27 +0800

    Refactor: Integrated twd2usd func to tw2us

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import name2WG
 import re
-import tw2us
+from tw2us import twd2usd
 
 def byLine_enditem_insert(inputSTR):
     if re.search(r'\(By',inputSTR) == None:
@@ -35,32 +35,7 @@ def reporter_name_insert(inputSTR):
     
     return inputSTR
 
-def twd2usd(inputSTR):
-    """Convert TWD to USD using the exchange rate."""
-    search_result = re.search(r'((NT\$[0-9\.,]+) (cents|million|billion|trillion){0,1})', inputSTR)
-    twdSTR = search_result.groups(0)[0]
-    # print(twdSTR)
-
-    if twdSTR:
-        units = ['cents', 'million', 'billion', 'trillion']
-        if any(unit in twdSTR for unit in units):
-            match = [unit for unit in units if unit in twdSTR]
-            if 'cents' in match:
-                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('cents', '')) / 100
-            elif 'million' in match:
-                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('million', '')) * 1_000_000
-            elif 'billion' in match:
-                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('billion', '')) * 1_000_000_000
-            elif 'trillion' in match:
-                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('trillion', '')) * 1_000_000_000_000
-            twdINT = int(twdINT)
-        else:
-            twdINT = int(twdSTR.strip().replace('NT$', '').replace(',', ''))
-        usdSTR = "{:,}".format(tw2us.twd2usd(twdINT))
-        inputSTR = inputSTR.replace(f'{twdSTR}', f'{twdSTR} (US${usdSTR}) ', 1)
-
-        
-    return inputSTR
+            
 
 if __name__ == '__main__':
     with open('test.txt', 'r',encoding='utf-8') as inputFile:
--- tw2us/__init__.py
+++ tw2us/__init__.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = "https://rate.bot.com.tw/xrt?Lang=zh-TW"
@@ -7,12 +8,37 @@ res.encoding = 'utf-8'
 
 soup = BeautifulSoup(res.text, "html.parser")
 
-def twd2usd(twdINT):
+def twd2usd(inputSTR):
     """Convert TWD to USD using the exchange rate."""
-    usd_rate = _get_usd_rate()
-    value = int(twdINT / usd_rate)
+    search_result = re.search(r'((NT\$[0-9\.,]+) (cents|million|billion|trillion){0,1})', inputSTR)
+    twdSTR = search_result.groups(0)[0]
+    # print(twdSTR)
 
-    return value
+    if twdSTR:
+        units = ['cents', 'million', 'billion', 'trillion']
+        if any(unit in twdSTR for unit in units):
+            match = [unit for unit in units if unit in twdSTR]
+            if 'cents' in match:
+                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('cents', '')) / 100
+            elif 'million' in match:
+                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('million', '')) * 1_000_000
+            elif 'billion' in match:
+                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('billion', '')) * 1_000_000_000
+            elif 'trillion' in match:
+                twdINT = float(twdSTR.strip().replace('NT$', '').replace(',', '').replace('trillion', '')) * 1_000_000_000_000
+            twdINT = int(twdINT)
+        else:
+            twdINT = int(twdSTR.strip().replace('NT$', '').replace(',', ''))
+
+        usd_rate = _get_usd_rate()
+        usd_value = int(twdINT / usd_rate)
+
+        usdSTR = "{:,}".format(usd_value)
+        inputSTR = inputSTR.replace(f'{twdSTR}', f'{twdSTR} (US${usdSTR}) ', 1)
+
+        
+    return inputSTR
+
 
 def _get_usd_rate():
     try:
